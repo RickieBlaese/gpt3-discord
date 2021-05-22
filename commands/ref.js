bf8e90b9e9782ad16d2c6b8a45f5c57d88120ea0
RoseChilds <rosechilds09@gmail.com>
2021-05-22 17:02:59 +0100

    ssssssssss

--- commands/ref.js
+++ commands/ref.js
@@ -7,7 +7,7 @@ module.exports = {
 		const Discord = client.Discord;
     client.database.getRef(message.author.id)
       .then(ref=>{
-        message.inlineReply(`Your referral code is \`${ref.code}\`. It has ${client.lib.thousands(ref.uses)} uses.\nGet users to invite the bot with your code by sending them this link: <https://honeybot.xyz/${ref.code}>.\nEvery invite you get will give you **250 tokens**. If the server has less than 25 members, you won't get a reward.`);
+        message.inlineReply(`Your referral code is \`${ref.code}\`. It has ${client.lib.thousands(ref.uses)} use${ref.uses==1 ? "" : "s"}.\nGet users to invite the bot with your code by sending them this link: <https://honeybot.xyz/${ref.code}>.\nEvery invite you get will give you **250 tokens**. If the server has less than 25 members, you won't get a reward.`);
       });
 	},
 };
