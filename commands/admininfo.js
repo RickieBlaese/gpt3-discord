3f2576145f75a1757960a26bfd6d9019183ccdce
RoseChilds <rosechilds09@gmail.com>
2021-05-24 21:40:34 +0100

    Admin info command

---
+++ commands/admininfo.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: 'admininfo',
+  cooldown: 0,
+  perms: 3,
+	description: 'Info that admin\'s can see (wow)',
+	indms: true,
+	execute(client, message, args) {
+    client.shard.fetchClientValues('guilds.cache.size')
+    	.then(results => {
+        message.inlineReply(`
+> Server count: ${client.lib.thousands(results.reduce((acc, guildCount) => acc + guildCount, 0))}
+> Shard count: ${client.lib.thousands(client.shard.ids.length)}`);
+    	});
+};
